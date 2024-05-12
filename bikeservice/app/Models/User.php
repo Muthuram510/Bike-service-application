b6bf2ec3771fbcc89f01ef1d49a618b109487026
Muthuram <muthuram5102002@gmail.com>
2024-05-12 13:09:36 +0530

    commit

--- bikeservice/app/Models/User.php
+++ bikeservice/app/Models/User.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -33,15 +36,19 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function blogs()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Service::class);
     }
 }
--- bikeservice/routes/web.php
+++ bikeservice/routes/web.php
@@ -1,7 +1,56 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::group(['prefix' => 'service'], function () {
+        Route::group(['middleware' => 'permission:service.write'], function () {
+            Route::get('/create', [ServiceController::class, 'create'])->name('service.create');
+            Route::post('', [ServiceController::class, 'store'])->name('service.store');
+            Route::put('/{service}', [ServiceController::class, 'update'])->name('service.update');
+            Route::delete('/{service}', [ServiceController::class, 'destroy'])->name('service.destroy');
+            Route::get('/{service}/edit', [ServiceController::class, 'edit'])->name('service.edit');
+        });
+        Route::group(['middleware' => 'permission:service.read|service.write'], function () {
+            Route::get('', [ServiceController::class, 'index'])->name('service.index');
+            Route::get('/{service}', [ServiceController::class, 'show'])->name('service.show');
+        });
+    });
+    Route::group(['prefix' => 'user'], function () {
+        Route::group(['middleware' => 'permission:users.write'], function () {
+            Route::get('/create', [UserController::class, 'create'])->name('user.create');
+            Route::get('/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
+            Route::post('', [UserController::class, 'store'])->name('user.store');
+            Route::put('/{user}', [UserController::class, 'update'])->name('user.update');
+            Route::delete('/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+        });
+        Route::group(['middleware' => 'permission:users.read|users.write'], function () {
+            Route::get('', [UserController::class, 'index'])->name('user.index');
+            Route::get('/{user}', [UserController::class, 'show'])->name('user.show');
+        });
+    });
 });
